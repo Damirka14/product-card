f665672714bb514407e50bdcf4f0cd036a26d57b
Damirka14 <rakhavella@icloud.com>
2026-04-25 15:01:19 +0500

    product cards using template

---
+++ products.js
@@ -0,0 +1,79 @@
+// Домашнее задание Уровень 1: № 8.2
+
+export const products = [
+  {
+    id: 1,
+    imgSrc: "./images/moisturizing-mousse.png",
+    imgAlt: "Увлажняющий мусс",
+    forSkin: "для нормальной кожи",
+    title: "Увлажняющий мусс",
+    description: "Глубоко увлажняют кожу лица, оставляя её мягкой и гладкой",
+    composition: [
+      "активные натуральные комплексы",
+      "витамины С, А, РР, В И Е",
+      "солнцезащитные компоненты"
+    ],
+    price: 2750,
+    currency: "₽"
+  },
+  {
+    id: 2,
+    imgSrc: "./images/moisturizing-mask.png",
+    imgAlt: "Увлажняющая маска",
+    forSkin: "для нормальной кожи",
+    title: "Увлажняющая маска",
+    description: "Способствует удерживанию влаги в верхних слоях кожи",
+    composition: [
+      "воски",
+      "минералы",
+      "масла"
+    ],
+    price: 3500,
+    currency: "₽"
+  },
+  {
+    id: 3,
+    imgSrc: "./images/face-wash.png",
+    imgAlt: "Гель для умывания",
+    forSkin: "для нормальной кожи",
+    title: "Гель для умывания",
+    description: "Интенсивно очищает, не повреждает защитный барьер кожи.",
+    composition: [
+      "минералы",
+      "витамины С, А, РР, В И Е",
+      "солнцезащитные компоненты"
+    ],
+    price: 1650,
+    currency: "₽"
+  },
+  {
+    id: 4,
+    imgSrc: "./images/gift-set-one.png",
+    imgAlt: "Подарочный набор №1",
+    forSkin: "для нормальной кожи",
+    title: "Подарочный набор №1",
+    description: "Набор, состоящий из увлажняющего крема и маски",
+    composition: [
+      "воски",
+      "минералы",
+      "масла"
+    ],
+    price: 4750,
+    currency: "₽"
+  },
+    {
+    id: 5,
+    imgSrc: "./images/gift-set-five.png",
+    imgAlt: "Подарочный набор №5",
+    forSkin: "для нормальной кожи",
+    title: "Подарочный набор №5",
+    description: "Весь набор средств Invisible symphony, крем, маска, мусс и гель для умывания",
+    composition: [
+      "воски",
+      "минералы",
+      "масла"
+    ],
+    price: 7520,
+    currency: "₽"
+  },
+];
