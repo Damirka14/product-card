a7423d98d2581775738df520b92b3b1c47bd8d73
Damirka14 <rakhavella@icloud.com>
2026-07-20 17:10:16 +0500

    modular structure. function context. classes.

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ import './homework-5.js';
 import './homework-6.js';
 import './homework-7.js';
 import './homework-8.js';
-import './homework-9.js';
+// import './homework-9.js';
 import './homework-10.js';
 
 console.log(' [Main] Все модули успешно загружены!');
